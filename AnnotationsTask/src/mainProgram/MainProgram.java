ae17aa89089edceee79cda98cc382e8f0a8a8049
KizerovDmitriy <113462277+KizerovDmitriy@users.noreply.github.com>
2023-02-15 12:52:24 +0200

    Update MainProgram.java

--- AnnotationsTask/src/mainProgram/MainProgram.java
+++ AnnotationsTask/src/mainProgram/MainProgram.java
@@ -1,14 +1,15 @@
-package mainProgram;
+package main;
 
 import classes.Calculate;
-import classes.ChildClass1;
 import classes.ChildClass2;
+
+import java.util.Map;
 
 
 public class MainProgram {
     public static void main(String[] args) {
         Calculate calculate = new Calculate();
-        calculate.countMethodAndParameters(ChildClass2.class);
-        System.out.println(calculate.getCounts());
+        Map<Class<?>, Integer> result = calculate.countMethodAndParameters(ChildClass2.class);
+        result.forEach((k,v)-> System.out.println(k + " : " + v));
     }
 }
