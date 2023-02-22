d0347259f3a3265033ca2ef102cf23338c89ddd5
KizerovDmitriy <113462277+KizerovDmitriy@users.noreply.github.com>
2023-02-22 21:07:03 +0200

    Update SerializableProgram.java

--- HomeTaskJavaProAnnotations/src/mainprograms/SerializableProgram.java
+++ HomeTaskJavaProAnnotations/src/mainprograms/SerializableProgram.java
@@ -8,8 +8,8 @@ public class SerializableProgram {
         SerializableClass serializableClass1 = new SerializableClass("Dmytro","Kizerov","32","0666334509");
         SerializableClass serializableClass2 = new SerializableClass();
         Saver saver = new Saver();
-        saver.serializeObject(serializableClass1,"C:\\Users\\Dmitriy\\Desktop\\HomeTaskJavaProAnnotations\\src\\resource\\serialize.txt");
-        saver.deserializeObject(serializableClass2,"C:\\Users\\Dmitriy\\Desktop\\HomeTaskJavaProAnnotations\\src\\resource\\serialize.txt");
+        saver.serializeObject(serializableClass1,args[0]);
+        saver.deserializeObject(serializableClass2,args[0]);
         System.out.println(serializableClass2);
     }
 }
