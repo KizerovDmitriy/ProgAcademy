ff7e34b4744b01e504e2b1c97be66fe60729f03f
KizerovDmitriy <113462277+KizerovDmitriy@users.noreply.github.com>
2023-03-12 14:53:05 +0200

    Solved

---
+++ Sql_task1/src/main/java/dao/ApartmentsDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import classes.Apartments;
+
+import java.util.List;
+
+public interface ApartmentsDAO {
+    void createTable();
+
+    void addApartments(String region, String address, Double area, Integer rooms, Double price);
+
+    List<Apartments> getApartments();
+
+    List<Apartments> getApartmentsByFilter(String filter);
+
+    List<Apartments> getApartmentsByFilter(Double filter);
+
+    List<Apartments> getApartmentsByFilter(Integer filter);
+}
