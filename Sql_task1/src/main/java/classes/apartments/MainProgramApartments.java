491ae7a54089d5acadb6076f9c40cce6b3e99c7f
KizerovDmitriy <113462277+KizerovDmitriy@users.noreply.github.com>
2023-03-15 12:51:34 +0200

    solved

--- Sql_task1/src/main/java/classes/apartments/MainProgramApartments.java
+++ Sql_task1/src/main/java/classes/apartments/MainProgramApartments.java
@@ -18,7 +18,7 @@ public class MainProgramApartments {
             System.out.println("Need to create new table? Y/N?");
             String answer = scanner.nextLine();
 
-            if (answer.equalsIgnoreCase("y")){
+            if (answer.equalsIgnoreCase("y")) {
                 dao.createTable();
             }
 
@@ -29,80 +29,18 @@ public class MainProgramApartments {
                 System.out.print("Input your choice --> ");
                 String choice = scanner.nextLine();
 
-
                 switch (choice) {
 
                     case "1":
-                        StringJoiner sj = new StringJoiner(",");
-                        System.out.println("Enter region:");
-                        sj.add(scanner.nextLine());
-                        System.out.println("Enter address:");
-                        sj.add(scanner.nextLine());
-                        System.out.println("Enter area of apartment:");
-                        sj.add(scanner.nextLine());
-                        System.out.println("Enter number of rooms:");
-                        sj.add(scanner.nextLine());
-                        System.out.println("Enter price:");
-                        sj.add(scanner.nextLine());
-                        String[] tmp = sj.toString().split(",");
-                        dao.addApartments(tmp[0], tmp[1], Double.parseDouble(tmp[2]), Integer.parseInt(tmp[3]), Double.parseDouble(tmp[4]));
+                        addApartmentsCase(scanner, dao);
                         break;
 
                     case "2":
-                        List<Apartments> apartments = dao.getApartments();
-                        if (apartments.isEmpty()) {
-                            System.out.println("NO DATA");
-                        } else {
-                            for (Apartments a :
-                                    apartments) {
-                                System.out.println(a);
-                            }
-                        }
+                        viewApartmentsCase(dao);
                         break;
 
                     case "3":
-                        System.out.println("Choose filer:");
-                        System.out.println("1: for filter by region or address");
-                        System.out.println("2: for filter by price or area of apartments");
-                        System.out.println("3: for filter by number of rooms");
-                        System.out.print("Input your choice --> ");
-                        String filterNumber = scanner.nextLine();
-                        System.out.print("Input filter parameter:  ");
-                        String filter = scanner.nextLine();
-                        switch (filterNumber) {
-                            case "1":
-                                List<Apartments> filterByRegionAndAddress = dao.getApartmentsByFilter(filter);
-                                if (filterByRegionAndAddress.isEmpty()) {
-                                    System.out.println("No matches");
-                                } else {
-                                    for (Apartments a : filterByRegionAndAddress) {
-                                        System.out.println(a);
-                                    }
-                                }
-                                break;
-                            case "2":
-                                List<Apartments> filterByAreaAndPrice = dao.getApartmentsByFilter(Double.parseDouble(filter));
-                                if (filterByAreaAndPrice.isEmpty()) {
-                                    System.out.println("No matches");
-                                } else {
-                                    for (Apartments a : filterByAreaAndPrice) {
-                                        System.out.println(a);
-                                    }
-                                }
-                                break;
-                            case "3":
-                                List<Apartments> filterByRooms = dao.getApartmentsByFilter(Integer.parseInt(filter));
-                                if (filterByRooms.isEmpty()) {
-                                    System.out.println("No matches");
-                                } else {
-                                    for (Apartments a : filterByRooms) {
-                                        System.out.println(a);
-                                    }
-                                }
-                                break;
-                            default:
-                                return;
-                        }
+                        if (getAllCase(scanner, dao)) return;
                         break;
                     default:
                         return;
@@ -111,5 +49,84 @@ public class MainProgramApartments {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static boolean getAllCase(Scanner scanner, ApartmentsDAO dao) {
+        System.out.println("Choose filer:");
+        System.out.println("1: for filter by region or address");
+        System.out.println("2: for filter by price or area of apartments");
+        System.out.println("3: for filter by number of rooms");
+        System.out.print("Input your choice --> ");
+        String filterNumber = scanner.nextLine();
+        System.out.print("Input filter parameter:  ");
+        String filter = scanner.nextLine();
+        if (filterCase(dao, filterNumber, filter)) return true;
+        return false;
+    }
+
+    private static void viewApartmentsCase(ApartmentsDAO dao) {
+        List<Apartments> apartments = dao.getApartments();
+        if (apartments.isEmpty()) {
+            System.out.println("NO DATA");
+        } else {
+            for (Apartments a :
+                    apartments) {
+                System.out.println(a);
+            }
+        }
+    }
+
+    private static boolean filterCase(ApartmentsDAO dao, String filterNumber, String filter) {
+        switch (filterNumber) {
+            case "1":
+                List<Apartments> filterByRegionAndAddress = dao.getApartmentsByFilter(filter);
+                if (filterByRegionAndAddress.isEmpty()) {
+                    System.out.println("No matches");
+                } else {
+                    for (Apartments a : filterByRegionAndAddress) {
+                        System.out.println(a);
+                    }
+                }
+                break;
+            case "2":
+                List<Apartments> filterByAreaAndPrice = dao.getApartmentsByFilter(Double.parseDouble(filter));
+                if (filterByAreaAndPrice.isEmpty()) {
+                    System.out.println("No matches");
+                } else {
+                    for (Apartments a : filterByAreaAndPrice) {
+                        System.out.println(a);
+                    }
+                }
+                break;
+            case "3":
+                List<Apartments> filterByRooms = dao.getApartmentsByFilter(Integer.parseInt(filter));
+                if (filterByRooms.isEmpty()) {
+                    System.out.println("No matches");
+                } else {
+                    for (Apartments a : filterByRooms) {
+                        System.out.println(a);
+                    }
+                }
+                break;
+            default:
+                return true;
+        }
+        return false;
+    }
+
+    private static void addApartmentsCase(Scanner scanner, ApartmentsDAO dao) {
+        StringJoiner sj = new StringJoiner(",");
+        System.out.println("Enter region:");
+        sj.add(scanner.nextLine());
+        System.out.println("Enter address:");
+        sj.add(scanner.nextLine());
+        System.out.println("Enter area of apartment:");
+        sj.add(scanner.nextLine());
+        System.out.println("Enter number of rooms:");
+        sj.add(scanner.nextLine());
+        System.out.println("Enter price:");
+        sj.add(scanner.nextLine());
+        String[] tmp = sj.toString().split(",");
+        dao.addApartments(tmp[0], tmp[1], Double.parseDouble(tmp[2]), Integer.parseInt(tmp[3]), Double.parseDouble(tmp[4]));
     }
 }
--- Sql_task1/src/main/java/classes/orders/MainOrders.java
+++ Sql_task1/src/main/java/classes/orders/MainOrders.java
@@ -25,72 +25,91 @@ public class MainOrders {
                 System.out.print("Input your choice --> ");
                 String choice = sc.nextLine();
 
-                switch (choice) {
-                    case "1":
-                        System.out.println("Input first name:");
-                        String firstName = sc.nextLine();
-                        System.out.println("Input last name:");
-                        String lastName = sc.nextLine();
-                        System.out.println("Input phone number:");
-                        String phoneNumber = sc.nextLine();
-                        dao.addClient(firstName, lastName, phoneNumber);
-                        break;
-                    case "2":
-                        System.out.println("Input product name:");
-                        String productName = sc.nextLine();
-                        System.out.println("Input price:");
-                        String price = sc.nextLine();
-                        System.out.println("Input quantity:");
-                        String quantity = sc.nextLine();
-                        dao.addNewProduct(productName, Double.parseDouble(price), Integer.parseInt(quantity));
-                        break;
-                    case "3":
-                        System.out.println("Input order details:");
-                        System.out.println("Input client name:");
-                        String name = sc.nextLine();
-                        System.out.println("Input client surname:");
-                        String surname = sc.nextLine();
-                        System.out.println("Input client phone number:");
-                        String phoneNum = sc.nextLine();
-                        Client client = new Client(name, surname, phoneNum);
-                        if (dao.checkClient(client) != null) {
-                            dao.addClient(client.getFirst_name(), client.getLast_name(), client.getPhone_number());
-                        }
-                        System.out.println("Input name of product:");
-                        String nameOfProduct = sc.nextLine();
-                        Product product = dao.checkProduct(nameOfProduct);
-                        if (product == null) {
-                            System.out.println("Product is not found!");
-                        } else {
-                            System.out.println("Input quantity:");
-                            int quantityOfProduct = sc.nextInt();
-                            dao.createNewOrder(dao.getClientByNPhoneNumber(client.getPhone_number()), product, quantityOfProduct);
-                        }
-                        break;
-                    case "4":
-                        Map<Integer, Client> clientList = dao.viewAllClients();
-                        for (Map.Entry<Integer, Client> clientEntry : clientList.entrySet()) {
-                            System.out.println(clientEntry.getValue());
-                        }
-                        break;
-                    case "5":
-                        Map<Integer, Product> productList = dao.viewAllProducts();
-                        for (Map.Entry<Integer, Product> productEntry : productList.entrySet()) {
-                            System.out.println(productEntry.getValue());
-                        }
-                        break;
-                    case "6":
-                        Map<Integer, Order> orderList = dao.viewAllOrders();
-                        for (Map.Entry<Integer, Order> order : orderList.entrySet()) {
-                            System.out.println(order.getValue());
-                        }
-                        break;
-                    default:
-                        return;
-                }
+                if (choiceCase(sc, dao, choice)) return;
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private static boolean choiceCase(Scanner sc, OrdersDAOImpl dao, String choice) {
+        switch (choice) {
+            case "1":
+                addClientCase(sc, dao);
+                break;
+            case "2":
+                addProductCase(sc, dao);
+                break;
+            case "3":
+                addOrderCase(sc, dao);
+                break;
+            case "4":
+                Map<Integer, Client> clientList = dao.viewAllClients();
+                for (Map.Entry<Integer, Client> clientEntry : clientList.entrySet()) {
+                    System.out.println(clientEntry.getValue());
+                }
+                break;
+            case "5":
+                Map<Integer, Product> productList = dao.viewAllProducts();
+                for (Map.Entry<Integer, Product> productEntry : productList.entrySet()) {
+                    System.out.println(productEntry.getValue());
+                }
+                break;
+            case "6":
+                Map<Integer, Order> orderList = dao.viewAllOrders();
+                for (Map.Entry<Integer, Order> order : orderList.entrySet()) {
+                    System.out.println(order.getValue());
+                }
+                break;
+            default:
+                return true;
+        }
+        return false;
+    }
+
+    private static void addOrderCase(Scanner sc, OrdersDAOImpl dao) {
+        System.out.println("Input order details:");
+        System.out.println("Input client name:");
+        String name = sc.nextLine();
+        System.out.println("Input client surname:");
+        String surname = sc.nextLine();
+        System.out.println("Input client phone number:");
+        String phoneNum = sc.nextLine();
+        Client client = new Client(name, surname, phoneNum);
+        if (dao.checkClient(client) != null) {
+            dao.addClient(client.getFirst_name(), client.getLast_name(), client.getPhone_number());
+        }
+        System.out.println("Input name of product:");
+        String nameOfProduct = sc.nextLine();
+        Product product = dao.checkProduct(nameOfProduct);
+        if (product == null) {
+            System.out.println("Product is not found!");
+        } else {
+            System.out.println("Input quantity:");
+            int quantityOfProduct = sc.nextInt();
+            dao.createNewOrder(dao.getClientByNPhoneNumber(client.getPhone_number()), product, quantityOfProduct);
+        }
+        return;
+    }
+
+    private static void addProductCase(Scanner sc, OrdersDAOImpl dao) {
+        System.out.println("Input product name:");
+        String productName = sc.nextLine();
+        System.out.println("Input price:");
+        String price = sc.nextLine();
+        System.out.println("Input quantity:");
+        String quantity = sc.nextLine();
+        dao.addNewProduct(productName, Double.parseDouble(price), Integer.parseInt(quantity));
+    }
+
+    private static void addClientCase(Scanner sc, OrdersDAOImpl dao) {
+        System.out.println("Input first name:");
+        String firstName = sc.nextLine();
+        System.out.println("Input last name:");
+        String lastName = sc.nextLine();
+        System.out.println("Input phone number:");
+        String phoneNumber = sc.nextLine();
+        dao.addClient(firstName, lastName, phoneNumber);
+        return;
     }
 }
