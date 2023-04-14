b3e749c48eda5fe36934dddd65d17bc50db1ee83
KizerovDmitriy <113462277+KizerovDmitriy@users.noreply.github.com>
2023-04-14 13:38:56 +0300

    Solved

---
+++ JavaProBot/src/main/java/academy/prog/javaprobot/UserService.java
@@ -0,0 +1,50 @@
+package academy.prog.javaprobot;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    @Transactional
+    public Optional<User> findUserById (long id){
+        return userRepository.findUserById(id);
+    }
+    @Transactional
+    public void deleteUser(long chatId){
+        userRepository.deleteUserByChatId(chatId);
+    }
+
+    @Transactional(readOnly = true)
+    public List<User> findAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable).getContent();
+    }
+
+    @Transactional(readOnly = true)
+    public long getUsersCount() {
+        return userRepository.count();
+    }
+
+    @Transactional
+    public void saveUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional
+    public void updateUser(User user) {
+        userRepository.save(user);
+    }
+
+    @Transactional(readOnly = true)
+    public Optional<User> findUserByChatId(long chatId) {
+        return userRepository.findByChatId(chatId);
+    }
+}
